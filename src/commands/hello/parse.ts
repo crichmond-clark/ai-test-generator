400adbf35df977dddca6e548b834692e25530de2
Connor Richmond-Clark <crichmondclark@outlook.com>
2024-09-04 10:37:32 +0100

    add first parsem command

---
+++ src/commands/hello/parse.ts
@@ -0,0 +1,33 @@
+import {Args, Command} from '@oclif/core';
+import {Project} from 'ts-morph';
+import * as path from 'path'; 
+
+export default class ParseFile extends Command {
+    static override args = {
+        file: Args.string({description: 'file to read', required: true})
+    }
+
+    static override description = 'this command will take a given file, find all functions and return a new file containing those functions'
+
+    public async run(): Promise<void> {
+        const {args} = await this.parse(ParseFile);
+
+        const project = new Project();
+        const functionsArray = extractFunctionsFromFile(project, args.file)
+
+
+        // Create a new source file
+        const outputFilePath = path.join(
+            path.dirname(args.file), 
+            `extracted_functions_${path.basename(args.file)}`
+        );
+        const outputFile = project.createSourceFile(outputFilePath);
+
+        const client = new GeminiClient('', JSON.stringify(functionsArray))
+        const tests = await client.generateTests()         
+        await outputFile.addStatements(tests)
+        await outputFile.save();
+
+        this.log(`Extracted functions saved to: ${outputFile.getFilePath()}`);
+    }
+}

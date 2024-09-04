486a890b35f988104faf53e7e03930f2616b90ed
Connor Richmond-Clark <crichmondclark@outlook.com>
2024-09-04 10:37:40 +0100

    create google gemini client wrapper

---
+++ src/gemini/client.ts
@@ -0,0 +1,13 @@
+export default class GeminiClient {
+  private GenAI: GoogleGenerativeAI
+
+  constructor(apiKey: string) {
+    this.GenAI = new GoogleGenerativeAI(apiKey)
+  }
+
+  async generateTests(prompt:string): Promise<string> {
+    const model = this.GenAI.getGenerativeModel({model: 'gemini-1.5-pro'})
+    const result = await model.generateContent
+    const response = await result.response
+    return response.text
+}}

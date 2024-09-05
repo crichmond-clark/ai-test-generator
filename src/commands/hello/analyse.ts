568a8e6bdff2f74cdb2d61d911954ae629b5be8f
Connor Richmond-Clark <crichmondclark@outlook.com>
2024-09-05 10:26:30 +0100

    add analysis command

---
+++ src/commands/hello/analyse.ts
@@ -0,0 +1,24 @@
+import {Args, Command} from '@oclif/core'
+import findUnTestedFunctions from '../../utils/findUntestedFunctions.js'
+
+export default class CountUntestedFunctions extends Command {
+  static override args = {
+    sourcePath: Args.string({description: 'source file or directory to analyze', required: true}),
+    testFolderPath: Args.string({description: 'path to the test folder', required: true}),
+  }
+
+  static override description = 'This command will count the number of untested functions in a given file or directory'
+
+  public async run(): Promise<void> {
+    const {args} = await this.parse(CountUntestedFunctions)
+
+    const untestedFunctionsMap = await findUnTestedFunctions(args.sourcePath, args.testFolderPath)
+
+    // Iterate over the map and print the results
+    for (const [functionName, filePaths] of untestedFunctionsMap) {
+      filePaths.forEach((filePath) => {
+        this.log(`${filePath} - 1`) // Assuming each function is in one file
+      })
+    }
+  }
+}
